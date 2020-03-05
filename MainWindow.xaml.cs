7ddac93b68a707621f83964ae2dcc5ca049d60ff
Zerumi <evgexa.2015@bk.ru>
2020-03-05 22:29:42 +0400

    assets support

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.Configuration;
+using System.Reflection;
 
 namespace CustomRPC
 {
@@ -37,15 +38,21 @@ namespace CustomRPC
 
         static System.Collections.Specialized.NameValueCollection settings = ConfigurationManager.AppSettings;
 
-        private string client_id = settings.Get("ApplicationID");
+        public string client_id = settings.Get("ApplicationID");
 
         DispatcherTimer timer = new DispatcherTimer();
 
-        DiscordRpcClient client;
+        public DiscordRpcClient client;
 
-        DateTime StartdateTime = default;
+        public DateTime StartdateTime = default;
 
-        DateTime EnddateTime = default;
+        public DateTime EnddateTime = default;
+
+        public string LargeImg = settings.Get("LargeImage");
+
+        public string SmallImg = settings.Get("SmallImage");
+
+        string sendname = null;
 
         RichPresence presence = new RichPresence();
 
@@ -96,7 +103,7 @@ namespace CustomRPC
 
         private void onReady(object _, ReadyMessage __)
         {
-            //timer.Start();
+            timer.Start();
             updateStatus();
         }
 
@@ -111,6 +118,14 @@ namespace CustomRPC
         private void TimerTick(object sender, EventArgs e)
         {
             updateStatus();
+            if (EnddateTime == default)
+            {
+                lTimestamp.Content = $"{(Convert.ToInt32(DateTime.UtcNow.Subtract(StartdateTime).Hours) == 0? Convert.ToString(Convert.ToInt32(DateTime.UtcNow.Subtract(StartdateTime).Minutes)) : Convert.ToString(Convert.ToString(Convert.ToInt32(DateTime.UtcNow.Subtract(StartdateTime).Hours))) + ":" + Convert.ToString(Convert.ToInt32(DateTime.UtcNow.Subtract(StartdateTime).Minutes)))}:{Convert.ToInt32(DateTime.UtcNow.Subtract(StartdateTime).Seconds)} elapsed";
+            }
+            else
+            {
+                //lTimestamp.Content = $"{(DateTime.Now.Subtract(StartdateTime).TotalHours == 0 ? Convert.ToString(DateTime.Now.Subtract(StartdateTime).TotalMinutes) : DateTime.Now.Subtract(StartdateTime).TotalHours + ":" + DateTime.Now.Subtract(StartdateTime).TotalMinutes)}:{DateTime.Now.Subtract(StartdateTime).TotalSeconds} left";
+            }
         }
 
         private void bUpdateStatus_Click(object sender, RoutedEventArgs e)
@@ -120,7 +135,7 @@ namespace CustomRPC
 
         private void bApply_Click(object sender, RoutedEventArgs e)
         {
-            UpdatePresence(StartdateTime, tbDetails.Text, tbState.Text, "", tbLargeImgText.Text, "", tbSmallImgText.Text);
+            UpdatePresence(StartdateTime, tbDetails.Text, tbState.Text, LargeImg, tbLargeImgText.Text, SmallImg, tbSmallImgText.Text);
         }
 
         private void cbSpectate_Checked(object sender, RoutedEventArgs e)
@@ -154,6 +169,7 @@ namespace CustomRPC
 
         private void LoadStatus()
         {
+            lGName.Content = "ApplicationID: " + settings.Get("ApplicationID");
             tbDetails.Text = settings.Get("Details");
             tbState.Text = settings.Get("State");
             bSetSpectate.IsEnabled = Convert.ToBoolean(settings.Get("boolSpectate"));
@@ -162,24 +178,93 @@ namespace CustomRPC
             cbAskToJoin.IsChecked = Convert.ToBoolean(settings.Get("boolAskToJoin"));
             tbSmallImgText.Text = settings.Get("SmallImageText");
             tbLargeImgText.Text = settings.Get("LargeImageText");
+            LargeImg = settings.Get("LargeImage");
+            SmallImg = settings.Get("SmallImage");
         }
 
         private void SaveStatus()
         {
-            settings.Set("ApplicationID", client.ApplicationID);
+            settings.Set("ApplicationID", client_id);
             settings.Set("Details", tbDetails.Text);
             settings.Set("State", tbState.Text);
             settings.Set("boolSpectate", Convert.ToString(cbSpectate.IsChecked.GetValueOrDefault()));
             settings.Set("boolAskToJoin", Convert.ToString(cbAskToJoin.IsChecked.GetValueOrDefault()));
-            settings.Set("LargeImage", "");
+            settings.Set("LargeImage", LargeImg);
             settings.Set("LargeImageText", tbLargeImgText.Text);
-            settings.Set("SmallImage", "");
+            settings.Set("SmallImage", SmallImg);
             settings.Set("SmallImageText", tbSmallImgText.Text);
+            var appSettings = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            appSettings.AppSettings.Settings.Clear();
+            for (int i = 0; i < settings.Count; i++)
+            {
+                appSettings.AppSettings.Settings.Add(settings.AllKeys[i], settings[i]);
+            }
+            appSettings.Save();
+        }
+
+        private void UpdateArg(object sender, string sendname)
+        {
+            lTitle.Content = (sender as Button).Content;
+            tbArgument.Visibility = Visibility.Visible;
+            bOK.Visibility = Visibility.Visible;
+            this.sendname = sendname;
         }
 
         private void bUpdateConfig_Click(object sender, RoutedEventArgs e)
         {
             SaveStatus();
+        }
+
+        private void bUpdateRPC_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateArg(sender, "AppID");
+        }
+
+        private void bUpdateLargeImg_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateArg(sender, "LargeImg");
+        }
+
+        private void bUpdateSmallImg_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateArg(sender, "SmallImg");
+        }
+
+        private void bUpdateTimestamp_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateArg(sender, "Timestamp");
+        }
+
+        private void bSetSpectate_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateArg(sender, "Spectate");
+        }
+
+        private void bSetAskToJoin_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateArg(sender, "AskToJoin");
+        }
+
+        private void bOK_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lTitle.Content = "";
+                tbArgument.Visibility = Visibility.Hidden;
+                bOK.Visibility = Visibility.Hidden;
+                var method = AppDomain.CurrentDomain.GetAssemblies()
+    .Select(x => x.GetTypes())
+    .SelectMany(x => x)
+    .Where(x => x.Namespace == "CustomRPC")
+    .Where(c => c.GetMethod(sendname) != null)
+    .Select(c => c.GetMethod(sendname)).First();
+                method.Invoke(null, new object[] { this });
+                tbArgument.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
---
+++ Update.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CustomRPC
+{
+    static class Update
+    {
+        public static void AppID(MainWindow window)
+        {
+            window.client_id = window.tbArgument.Text;
+            MessageBox.Show("Для применения изменений перезагрузите программу (сохраните конфигурацию)", "Custom RPC", MessageBoxButton.OK, MessageBoxImage.Exclamation, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+        }
+        public static void LargeImg(MainWindow window)
+        {
+            window.LargeImg = window.tbArgument.Text;
+        }
+        public static void SmallImg(MainWindow window)
+        {
+            window.SmallImg = window.tbArgument.Text;
+        }
+        public static void Timestamp(MainWindow window)
+        {
+            
+        }
+        public static void Spectate(MainWindow window)
+        {
+
+        }
+        public static void AskToJoin(MainWindow window)
+        {
+
+        }
+    }
+}
